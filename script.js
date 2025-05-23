ce856585091f45caabca3dcd4b6a4d9a6a9d998b
anuj-bhakat <developer.anuj.ab@gmail.com>
2025-05-23 10:56:09 +0530

    23-May

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const now=new Date();
-const time=now.toLocaleTimeString("en-US");
+// const time=now.toLocaleTimeString("en-US");
+const hours = String(now.getHours()).padStart(2, '0');
+const minutes = String(now.getMinutes()).padStart(2, '0');
+
+const time=`${hours}:${minutes}`;
+
+
 const dayOfWeek=now.getDay();
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const currentDay = days[dayOfWeek];
